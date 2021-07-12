7056c18d6b24b61a2ed8fa31579c91f1548e41a6
Martin Voelkl <martin.andreas.volkl@cern.ch>
2021-07-12 16:34:28 +0200

    Solenoid tracker

---
+++ build/CMakeFiles/exampleB2a.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/exampleB2a.cc" "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/build/CMakeFiles/exampleB2a.dir/exampleB2a.cc.o"
+  "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/src/B2ActionInitialization.cc" "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/build/CMakeFiles/exampleB2a.dir/src/B2ActionInitialization.cc.o"
+  "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/src/B2EventAction.cc" "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/build/CMakeFiles/exampleB2a.dir/src/B2EventAction.cc.o"
+  "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/src/B2PrimaryGeneratorAction.cc" "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/build/CMakeFiles/exampleB2a.dir/src/B2PrimaryGeneratorAction.cc.o"
+  "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/src/B2RunAction.cc" "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/build/CMakeFiles/exampleB2a.dir/src/B2RunAction.cc.o"
+  "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/src/B2TrackerHit.cc" "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/build/CMakeFiles/exampleB2a.dir/src/B2TrackerHit.cc.o"
+  "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/src/B2TrackerSD.cc" "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/build/CMakeFiles/exampleB2a.dir/src/B2TrackerSD.cc.o"
+  "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/src/B2aDetectorConstruction.cc" "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/build/CMakeFiles/exampleB2a.dir/src/B2aDetectorConstruction.cc.o"
+  "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/src/B2aDetectorMessenger.cc" "/home/martin/Documents/LowTheoremStudies/DetectorSimulation/Solenoid/SolenoidSimulation/build/CMakeFiles/exampleB2a.dir/src/B2aDetectorMessenger.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/martin/root/GeantLocal/geant4.10.07-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ include/B2Analysis.hh
@@ -0,0 +1,8 @@
+#ifndef B2Analysis_h
+#define B2Analysis_h 1
+
+//#include "g4root.hh"
+//#include "g4xml.hh"
+#include "g4csv.hh" // can be used only with ntuples
+
+#endif
